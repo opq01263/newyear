74f741486ffa44b1a624058edfee0d8aed31e76b
opq01263 <46513117+opq01263@users.noreply.github.com>
2019-02-08 17:59:19 +0800

    new

--- js/all.js
+++ js/all.js
@@ -3,6 +3,11 @@ jQuery(document).ready(function($) {
 	$(".enter").click(function(event) {
 		$("h1,p").slideDown(500)
 		$(".enter").hide()
-
+$(function(){
+    setInterval(flicker,1000);//迴圈閃爍，間隔1秒
+})
+function flicker(){//閃爍函數
+    $('p').fadeOut(500).fadeIn(500);
+}
 	});
 });
