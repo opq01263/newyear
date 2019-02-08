09af0087f0c0ed49551c35ef7c0d16b8c348fba1
opq01263 <46513117+opq01263@users.noreply.github.com>
2019-02-08 18:01:12 +0800

    new

--- js/all.js
+++ js/all.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($) {
 		$("h1,p").slideDown(500)
 		$(".enter").hide()
 $(function(){
-    setInterval(flicker,1000);//迴圈閃爍，間隔1秒
+    setInterval(flicker,500);//迴圈閃爍，間隔1秒
 })
 function flicker(){//閃爍函數
     $('p').fadeOut(500).fadeIn(500);
