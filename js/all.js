47eade91f5db24839d5d74a7c7b8f82b57672f5f
opq01263 <46513117+opq01263@users.noreply.github.com>
2019-02-10 12:32:56 +0800

    new

--- js/all.js
+++ js/all.js
@@ -1,13 +1,17 @@
-jQuery(document).ready(function($) {
-	$("h1,p").hide()
-	$(".enter").click(function(event) {
-		$("h1,p").slideDown(500)
-		$(".enter").hide()
-$(function(){
-    setInterval(flicker,500);//迴圈閃爍，間隔1秒
-})
-function flicker(){//閃爍函數
-    $('p').fadeOut(500).fadeIn(500);
-}
-	});
+$(document).ready(function() {
+
+  $('.question h1').click(function(event) {
+
+    // 讓點擊到的 h3 亮起來，其他h3移除active樣式
+    $(this).toggleClass('active');
+
+    // 讓點擊到的 h3找到父元素 .question ，再找裡面的 P 判斷收闔
+    $(this).parent().find('p').slideToggle();
+
+    // 自己以外的 p 隱藏起來
+    $(this).parent().siblings().find('p').slideUp();
+    // 自己以外的 h3 移除u樣式
+    $(this).parent().siblings().find('h1').removeClass('active');
+
+  });
 });
